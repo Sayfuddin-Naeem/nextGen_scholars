3bff4058715f072a6cb97f70c748b7b74837f71f
Sayfuddin-Naeem <sayfuddinnaeem.cse4.bu@gmail.com>
2024-09-10 07:02:18 +0600

    Changes aplied

--- nextGen_scholars/settings.py
+++ nextGen_scholars/settings.py
@@ -68,10 +68,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:8000",
-    "http://localhost:8000",
-]
+CORS_ALLOW_ALL_ORIGINS = True
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://127.0.0.1:8000",
+#     "http://localhost:8000",
+# ]
 
 CORS_ALLOW_HEADERS = [
     'content-type',
